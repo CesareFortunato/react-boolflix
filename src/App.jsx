1e32bf1e31d4eb057ee2e7047fab7799246d1c1c
Cesare89 <cf@nume.plus>
2026-02-03 11:50:23 +0100

    feat: add movie search + flags + img poster

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,48 @@ import { useState } from 'react'
 import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const endpoint = "https://api.themoviedb.org/3/search/tv?api_key="
+const endpointTV = "https://api.themoviedb.org/3/search/tv?api_key="
+const endpointMovie = "https://api.themoviedb.org/3/search/movie?api_key="
 
 function App() {
   const [searchInput, setSearchInput] = useState("");
   const [tvResults, setTvResults] = useState([]);
+  const [movieResults, setMovieResults] = useState([]);
 
   function handleSearch() {
 
     axios
-      .get(endpoint + API_KEY + "&query=" + searchInput)
+      .get(endpointMovie + API_KEY + "&query=" + searchInput)
       .then(response => {
-        setTvResults(response.data)
+        setMovieResults(response.data.results)
       })
+      .catch(err => {
+        console.log(err);
+      });
+
+    axios
+      .get(endpointTV + API_KEY + "&query=" + searchInput)
+      .then(response => {
+        setTvResults(response.data.results)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  function languageToCountry(lang) {
+    const map = {
+      en: "gb",
+      it: "it",
+      fr: "fr",
+      es: "es",
+      de: "de",
+      ja: "jp",
+      ko: "kr",
+      zh: "cn"
+    };
+
+    return map[lang] || null;
   }
 
   return (
@@ -34,15 +63,54 @@ function App() {
               Cerca
             </button>
           </div>
+          <h1>Serie TV</h1>
           <ul>
-            {tvResults.map(el =>
-            (<li key={el.id}>
-              <p>{el.name}</p>
-              <p>{el.original_name}</p>
-              <p>{el.original_language}</p>
-              <p>{el.vote_average}</p>
-            </li>)
-            )}
+            {tvResults.map(el => {
+
+              const countryCode = languageToCountry(el.original_language);
+              return (
+                <li key={el.id}>
+                  <p>{el.name}</p>
+                  <p>{el.original_name}</p>
+                  {countryCode ? (
+                    <span className={`fi fi-${countryCode}`}
+                      style={{ width: "24px", height: "16px" }}
+                    ></span>
+                  ) : (
+                    <span>{el.original_language}</span>
+                  )}
+                  <p>{el.vote_average}</p>
+                </li>
+              )
+            })}
+          </ul>
+          <h1>Film</h1>
+          <ul>
+            {movieResults.map(el => {
+
+              const countryCode = languageToCountry(el.original_language);
+              return (
+                <li key={el.id}>
+                  <img
+                    src={
+                      el.poster_path
+                        ? `https://image.tmdb.org/t/p/w342${el.poster_path}`
+                        : "https://via.placeholder.com/342x513?text=No+Image"
+                    }
+                    alt={el.name}/>
+                  <p>{el.title}</p>
+                  <p>{el.original_title}</p>
+                  {countryCode ? (
+                    <span className={`fi fi-${countryCode}`}
+                      style={{ width: "24px", height: "16px" }}
+                    ></span>
+                  ) : (
+                    <span>{el.original_language}</span>
+                  )}
+                  <p>{el.vote_average}</p>
+                </li>
+              )
+            })}
           </ul>
         </div>
       </div>
