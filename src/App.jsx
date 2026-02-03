5886d8898dd10a0556d6951659567c1079942d93
Cesare89 <cf@nume.plus>
2026-02-03 10:21:23 +0100

    feat: axios call to api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
 import { useState } from 'react'
+import axios from 'axios';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const endpoint = "https://api.themoviedb.org/3/search/tv?api_key="
 
 function App() {
   const [searchInput, setSearchInput] = useState("");
 
   function handleSearch() {
 
-    console.log(searchInput);
+    axios
+      .get(endpoint + API_KEY + "&query=" + searchInput)
+      .then(response => {
+        console.log(response.data.results)
+      })
   }
 
   return (
 
-    <div className="container mt-5"> <div className="row"> <div className="col-12"> <div className="input-group">
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-12">
+          <div className="input-group">
 
-      <input type="text" value={searchInput} onChange={e => setSearchInput(e.target.value)} className="form-control" placeholder="Cerca..." />
+            <input type="text" value={searchInput}
+              onChange={e => setSearchInput(e.target.value)}
+              className="form-control"
+              placeholder="Cerca..." />
 
-      <button className="btn btn-dark" onClick={handleSearch}> Cerca </button> </div> </div> </div> </div>
+            <button className="btn btn-dark"
+              onClick={handleSearch}>
+              Cerca
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
 
   );
 
