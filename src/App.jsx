6d0a47fa664186c48cb1ad5f876fc66c8561b96e
Cesare89 <cf@nume.plus>
2026-02-03 09:50:54 +0100

    feat: milestone 1, searchbar, onClick with console log

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-
+import { useState } from 'react'
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
 
+  function handleSearch() {
 
-  return 
+    console.log(searchInput);
+  }
+
+  return (
+
+    <div className="container mt-5"> <div className="row"> <div className="col-12"> <div className="input-group">
+
+      <input type="text" value={searchInput} onChange={e => setSearchInput(e.target.value)} className="form-control" placeholder="Cerca..." />
+
+      <button className="btn btn-dark" onClick={handleSearch}> Cerca </button> </div> </div> </div> </div>
+
+  );
+
 }
 
 export default App
